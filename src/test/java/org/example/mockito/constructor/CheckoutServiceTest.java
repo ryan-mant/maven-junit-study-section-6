5f41e45e5db292a6766cef7608451abcb7a42d72
ryan-mant <ryan.tuglife@hotmail.com>
2025-07-19 12:41:22 -0300

    mocking static method with constructors

---
+++ src/test/java/org/example/mockito/constructor/CheckoutServiceTest.java
@@ -0,0 +1,32 @@
+package org.example.mockito.constructor;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class CheckoutServiceTest {
+
+    @Test
+    void testMockObjectConstruction() {
+        //Given
+        try (MockedConstruction<PaymentProcessor> mocked =
+                     mockConstruction(PaymentProcessor.class,
+                             (mock, context) -> when(mock.chargeCustomer(
+                                     anyString(),
+                                     any(BigDecimal.class))).thenReturn(BigDecimal.TEN))) {
+
+
+            CheckoutService service = new CheckoutService();
+            // When
+            BigDecimal result = service.purchaseProduct("MacBook Pro", "42");
+
+            // Then
+            assertEquals(BigDecimal.TEN, result);
+            assertEquals(1, mocked.constructed().size());
+        }
+    }
+}
